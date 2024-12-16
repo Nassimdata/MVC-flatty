7724c20133cc3aa05c7465bec86cd8b33024d756
andy <luuandy94@gmail.com>
2024-12-16 16:12:03 +0100

    m

--- FLATTY/vue/page_accueil.php
+++ FLATTY/vue/page_accueil.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();  // Démarre la session
+session_start();  // Démarre la session m
 
 // Vérifier si l'utilisateur est connecté
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
